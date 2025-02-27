db57f82482cc3d47245e9702b1263a0427734e25
Ricko Caesar Aprilla Tiaka <78606675+younixue3@users.noreply.github.com>
2025-02-27 15:15:58 +0800

    fix design

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -3,10 +3,8 @@ import {
   ChevronDown,
   CircleUser,
   Factory,
-  GalleryThumbnails,
   Home,
   Menu,
-  Newspaper,
   NewspaperIcon,
   Package,
   Search,
@@ -41,100 +39,58 @@ export default function Authenticated({
   };
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+    <div className="grid min-h-screen w-full md:grid-cols-[260px_1fr] lg:grid-cols-[300px_1fr] bg-gray-100">
+      <div className="hidden h-screen border-r bg-blue-900 text-white md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <div className="flex h-16 items-center border-b border-blue-700 px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
-              <ApplicationLogo className="h-10 w-10 fill-current text-gray-500" />
-              <span>Satu Data UKM</span>
+              <ApplicationLogo className="h-10 w-10 fill-current text-white" />
+              <span className="text-lg">Satu Data UKM</span>
             </Link>
           </div>
           <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <nav className="grid items-start px-4 text-sm font-medium">
               <Link
                 href={route('dashboard.index')}
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-white transition-all hover:bg-blue-700"
               >
-                <Home className="h-4 w-4" />
+                <Home className="h-5 w-5" />
                 Dashboard
               </Link>
               <Link
                 href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-white transition-all hover:bg-blue-700"
               >
-                <Package className="h-4 w-4 mr-1" />
+                <Package className="h-5 w-5" />
                 Publikasi
               </Link>
               <Collapsible>
-                <CollapsibleTrigger className="flex w-full items-center justify-between gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+                <CollapsibleTrigger className="flex w-full items-center justify-between gap-4 rounded-lg px-3 py-2 text-white hover:bg-blue-700">
                   <div className="flex items-center gap-3">
-                    <NewspaperIcon className="h-4 w-4 mr-1" />
+                    <NewspaperIcon className="h-5 w-5" />
                     Post
                   </div>
-                  <ChevronDown className="h-4 w-4" />
+                  <ChevronDown className="h-5 w-5" />
                 </CollapsibleTrigger>
-                <CollapsibleContent className="px-4 py-2">
+                <CollapsibleContent className="px-6 py-2 bg-blue-800 rounded-lg">
                   <div className="grid gap-2">
-                    {/* <Link
-                      href="#"
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                    >
-                      <GalleryThumbnails className="h-4 w-4 mr-1" />
-                      Gallery
-                    </Link> */}
                     <Link
                       href={route('dashboard.news.index')}
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-white transition-all hover:bg-blue-700"
                     >
-                      <NewspaperIcon className="h-4 w-4 mr-1" />
                       News
                     </Link>
                     <Link
                       href={route('dashboard.profil.index')}
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-white transition-all hover:bg-blue-700"
                     >
-                      <NewspaperIcon className="h-4 w-4 mr-1" />
                       Profil
                     </Link>
                     <Link
                       href={route('dashboard.bidang.index')}
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-white transition-all hover:bg-blue-700"
                     >
-                      <NewspaperIcon className="h-4 w-4 mr-1" />
                       Bidang
-                    </Link>
-                  </div>
-                </CollapsibleContent>
-              </Collapsible>
-              <Link
-                href={route('dashboard.user.index')}
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-              >
-                <Users className="h-4 w-4 mr-1" />
-                Manajemen User
-              </Link>
-              <Collapsible>
-                <CollapsibleTrigger className="flex w-full items-center justify-between gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                  <div className="flex items-center gap-3">
-                    <Factory className="h-4 w-4 mr-1" />
-                    Industri
-                  </div>
-                  <ChevronDown className="h-4 w-4" />
-                </CollapsibleTrigger>
-                <CollapsibleContent className="px-4 py-2">
-                  <div className="grid gap-2">
-                    <Link
-                      href={route('dashboard.ikm.index')}
-                      className="flex items-center gap-4 rounded-lg px-3 py-2 text-muted-foreground hover:text-foreground"
-                    >
-                      IKM
-                    </Link>
-                    <Link
-                      href={route('dashboard.bigindustri.index')}
-                      className="flex items-center gap-4 rounded-lg px-3 py-2 text-muted-foreground hover:text-foreground"
-                    >
-                      Big Industri
                     </Link>
                   </div>
                 </CollapsibleContent>
@@ -144,14 +100,10 @@ export default function Authenticated({
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="flex h-16 items-center gap-4 border-b bg-white px-6 shadow-md">
           <Sheet>
             <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden"
-              >
+              <Button variant="outline" size="icon" className="md:hidden">
                 <Menu className="h-5 w-5" />
                 <span className="sr-only">Toggle navigation menu</span>
               </Button>
@@ -160,110 +112,65 @@ export default function Authenticated({
               <nav className="grid gap-2 p-4 py-6 text-lg font-medium">
                 <Link
                   href="/"
-                  className="flex items-center gap-2 text-lg w-full font-semibold"
+                  className="flex items-center gap-2 text-lg w-full font-semibold text-blue-900"
                 >
                   <img
                     alt="Logo Kota Samarinda"
                     src="/images/logo-kota-samarinda.png"
                     className="h-10 w-10"
                   />
-                  <span className="text-black text-sm">SatuData UKM</span>
+                  <span>SatuData UKM</span>
                 </Link>
-                <Link
-                  href={route('dashboard.index')}
-                  className="flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Home className="h-5 w-5" />
-                  Dashboard
-                </Link>
-                <Link
-                  href="#"
-                  className="flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Package className="h-5 w-5" />
-                  Publikasi
-                </Link>
-                <Link
-                  href="#"
-                  className="flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Ticket className="h-5 w-5" />
-                  Gallery
-                </Link>
-                <Link
-                  href={route('dashboard.user.index')}
-                  className="flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Users className="h-5 w-5" />
-                  Manajemen User
-                </Link>
-                <Collapsible>
-                  <CollapsibleTrigger className="flex w-full items-center justify-between gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                    <div className="flex items-center gap-4">
-                      <Factory className="h-5 w-5" />
-                      <span>Industri</span>
-                    </div>
-                    <ChevronDown className="h-4 w-4" />
-                  </CollapsibleTrigger>
-                  <CollapsibleContent className="px-4 py-2">
-                    <div className="grid gap-2">
-                      <Link
-                        href={route('dashboard.ikm.index')}
-                        className="flex items-center gap-4 rounded-lg px-3 py-2 text-muted-foreground hover:text-foreground"
-                      >
-                        Industri Kecil Menengah
-                      </Link>
-                      <Link
-                        href={route('dashboard.bigindustri.index')}
-                        className="flex items-center gap-4 rounded-lg px-3 py-2 text-muted-foreground hover:text-foreground"
-                      >
-                        Industri Besar
-                      </Link>
-                    </div>
-                  </CollapsibleContent>
-                </Collapsible>
               </nav>
             </SheetContent>
           </Sheet>
           <div className="w-full flex-1">
             <form>
               <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Search className="absolute left-2.5 top-2.5 h-5 w-5 text-gray-500" />
                 <Input
                   type="search"
                   placeholder="Search..."
-                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                  className="w-full bg-gray-100 pl-10 shadow-none md:w-2/3 lg:w-1/3"
                 />
               </div>
             </form>
           </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="secondary" size="icon" className="rounded-full">
+              <Button
+                variant="secondary"
+                size="icon"
+                className="rounded-full bg-blue-900 text-white"
+              >
                 <CircleUser className="h-5 w-5" />
                 <span className="sr-only">Toggle user menu</span>
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>
-                <div className="font-bold">{auth.user.name}</div>
-                <div className="text-xs text-muted-foreground">
-                  {auth.user.email}
-                </div>
+                <div className="font-bold text-gray-800">{auth.user.name}</div>
+                <div className="text-xs text-gray-500">{auth.user.email}</div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem asChild>
-                <Link href={route('logout')} method="post" className="w-full">
+                <Link
+                  href={route('logout')}
+                  method="post"
+                  className="w-full text-red-500"
+                >
                   Logout
                 </Link>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="flex-1 p-6">{children}</main>
+        <main className="flex-1 p-6 bg-white shadow-sm rounded-lg">
+          {children}
+        </main>
       </div>
       <Toaster />
     </div>
